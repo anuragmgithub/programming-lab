a1f352d9f5bfa85b0611638503211cf9012aad50
Anurag Mishra <mishra.anurag643@gmail.com>
2025-12-07 00:11:00 +0530

    data class slots

--- POC/data_cls_slots.py
+++ POC/data_cls_slots.py
@@ -6,7 +6,17 @@ class ProductNoSlots:
     name: str
     price: float
 
+@dataclass(slots=True)
+class ProductSlots:
+    id: int
+    name: str
+    price: float
+
 
 p = ProductNoSlots(1, "Laptop", 50000)
 print(p.__dict__)   # visible and modifiable
 p.new_field = "extra"  # allowed
+
+ps = ProductSlots(2, "Mobile", 25000)
+print(ps.__dict__)  # AttributeError: 'ProductSlots' object has no attribute '__dict__'
+# ps.new_field = "extra"  # AttributeError: 'ProductSlots
